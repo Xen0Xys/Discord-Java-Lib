41fcc812b98482d2372ccdb43d940260db290a61
Czekaj Tom <red57101@gmail.com>
2023-03-02 14:59:56 +0100

    :art: Rename main app class

--- src/main/java/fr/xen0xys/discordjava/DJApp.java
+++ src/main/java/fr/xen0xys/discordjava/DJApp.java
@@ -1,22 +1,20 @@
 package fr.xen0xys.discordjava;
 
-import fr.xen0xys.discordjava.commands.AbstractSlashCommand;
-import fr.xen0xys.discordjava.commands.CommandsManager;
-import fr.xen0xys.discordjava.components.ComponentsManager;
+import fr.xen0xys.discordjava.components.managers.CommandsManager;
+import fr.xen0xys.discordjava.components.managers.ComponentsManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.session.ShutdownEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
-public class DJBot extends ListenerAdapter {
+public class DJApp extends ListenerAdapter {
 
     private final JDA jda;
     private Logger logger;
@@ -24,9 +22,7 @@ public class DJBot extends ListenerAdapter {
     private final CommandsManager commandsManager;
     private final ComponentsManager componentsManager;
 
-    private final List<AbstractSlashCommand> commands = new ArrayList<>();
-
-    public DJBot(@NotNull String token, @NotNull String loggerName) throws InterruptedException {
+    public DJApp(@NotNull final String token, @NotNull final String loggerName) throws InterruptedException {
         this.setupLogger(loggerName);
         this.jda = JDABuilder.createDefault(token).build().awaitReady();
         this.commandsManager = new CommandsManager(this);
@@ -34,14 +30,25 @@ public class DJBot extends ListenerAdapter {
         jda.addEventListener(this);
     }
 
-    private void setupLogger(@NotNull String loggerName){
+    private void setupLogger(@NotNull final String loggerName){
         try {
-            InputStream stream = DJBot.class.getClassLoader().getResourceAsStream("logging.properties");
+            InputStream stream = DJApp.class.getClassLoader().getResourceAsStream("logging.properties");
             LogManager.getLogManager().readConfiguration(stream);
             this.logger = Logger.getLogger(loggerName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void stop(){
+        this.jda.shutdown();
+    }
+
+    @Override
+    public void onShutdown(@NotNull final ShutdownEvent event) {
+        if(this.componentsManager.getHandledComponents().size() > 0)
+            this.logger.warning("There are still handled components, data will be lost!");
+        this.logger.info("DJApp shutdown!");
     }
 
     public JDA getJDA() {
