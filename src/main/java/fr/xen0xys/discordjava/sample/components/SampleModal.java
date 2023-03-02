5e2a507bdd280e225f22ef7888bbe690b967298b
Czekaj Tom <red57101@gmail.com>
2023-03-02 15:00:13 +0100

    :memo: Add sample code for all features

--- src/main/java/fr/xen0xys/discordjava/sample/components/SampleModal.java
+++ src/main/java/fr/xen0xys/discordjava/sample/components/SampleModal.java
@@ -1,13 +1,16 @@
-package fr.xen0xys.discordjava.sample;
+package fr.xen0xys.discordjava.sample.components;
 
-import fr.xen0xys.discordjava.DJBot;
+import fr.xen0xys.discordjava.DJApp;
 import fr.xen0xys.discordjava.components.modal.AbstractModal;
 import fr.xen0xys.discordjava.components.modal.ModalField;
 import fr.xen0xys.discordjava.utils.IdUtils;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class SampleModal extends AbstractModal {
 
@@ -17,19 +20,25 @@ public class SampleModal extends AbstractModal {
 
     private final IdUtils idUtils;
 
-    public SampleModal(DJBot bot, long targetChannelId) {
+    public SampleModal(DJApp bot, long targetChannelId) {
         super("sample_modal", "Sample modal", modalField);
         this.targetChannelId = targetChannelId;
         this.idUtils = new IdUtils(bot);
     }
 
     @Override
-    public void callback(@NotNull DJBot bot, @NotNull ModalInteractionEvent e) {
+    public void callback(@NotNull DJApp bot, @NotNull ModalInteractionEvent e) {
         TextChannel targetChannel = this.idUtils.getTextChannelFromId(this.targetChannelId);
         if(targetChannel != null){
-            targetChannel.sendMessage("Hello " + e.getValues().get(0).getAsString()).queue();
+            targetChannel.sendMessage("Hello " + e.getValues().get(0).getAsString()).addActionRow(this.getButton(bot, e.getUser().getIdLong())).queue();
             e.deferReply(true).addContent("Message sent").queue();
         }else
             e.deferReply(true).addContent("The target channel is not found").queue();
+    }
+
+    private Button getButton(DJApp bot, long userId){
+        SampleButton button = new SampleButton();
+        button.handle(bot.getComponentsManager(), List.of(userId));
+        return button.getButton();
     }
 }
--- src/main/java/fr/xen0xys/discordjava/sample/components/SampleSlashCommand.java
+++ src/main/java/fr/xen0xys/discordjava/sample/components/SampleSlashCommand.java
@@ -1,13 +1,14 @@
-package fr.xen0xys.discordjava.sample;
+package fr.xen0xys.discordjava.sample.components;
 
-import fr.xen0xys.discordjava.DJBot;
-import fr.xen0xys.discordjava.commands.AbstractSlashCommand;
-import fr.xen0xys.discordjava.commands.SlashCommandOption;
+import fr.xen0xys.discordjava.DJApp;
+import fr.xen0xys.discordjava.components.commands.AbstractSlashCommand;
+import fr.xen0xys.discordjava.components.commands.SlashCommandOption;
 import fr.xen0xys.discordjava.components.modal.AbstractModal;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Objects;
 
 public class SampleSlashCommand extends AbstractSlashCommand {
@@ -18,10 +19,10 @@ public class SampleSlashCommand extends AbstractSlashCommand {
     }
 
     @Override
-    public void callback(@NotNull DJBot bot, @NotNull SlashCommandInteraction e) {
+    public void callback(@NotNull DJApp bot, @NotNull SlashCommandInteraction e) {
         long targetChannelId = Objects.requireNonNull(e.getOption("channel")).getAsLong();
         AbstractModal modal = new SampleModal(bot, targetChannelId);
-        modal.register(bot, e.getUser().getIdLong());
+        modal.handle(bot.getComponentsManager(), List.of(e.getUser().getIdLong()));
         e.replyModal(modal.getModal()).queue();
     }
 }
